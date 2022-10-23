9026b1de5dede1cd2a1d46276c489bc0f955969b
nbouml <boum.nour19@gmail.com>
2022-10-23 11:42:55 +0200

    new: param of the logger

---
+++ set_logging.py
@@ -0,0 +1,26 @@
+import logging
+from os import remove
+from os.path import exists
+
+
+def setup_logger(logger_name, log_file, level=logging.WARNING):
+    # Erase log if already exists
+    if exists(log_file):
+        remove(log_file)
+    # Configure log file
+    l = logging.getLogger(logger_name)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fileHandler = logging.FileHandler(log_file, mode='w')
+    fileHandler.setFormatter(formatter)
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
+    l.setLevel(level)
+    l.addHandler(fileHandler)
+    l.addHandler(streamHandler)
+
+
+if __name__ == '__main__':
+    setup_logger('log', 'Atest.log', logging.DEBUG)
+    log = logging.getLogger('log')
+    log.info('TEST')
+    log.debug('TEST')
