4b5b4d923176e04dca8ab1f0bff34bcc09058d4d
nbouml <boum.nour19@gmail.com>
2022-10-23 21:47:52 +0200

    fix: wip

--- drugs_graph/src/drugs_graph.py
+++ drugs_graph/src/drugs_graph.py
@@ -10,6 +10,6 @@ df_ct = ut.get_df(s.clinical_trials_file, {'index_col': 0})
 df_dr = ut.get_df(s.drugs_file, {'index_col': 0})
 df_pub = ut.get_df(s.pubmed_file, {'index_col': 0})
 
-df_ct = ut.check_for_matching_if_nan(df_ct, s.cols_ref_ct)
+# df_ct = ut.check_for_matching_if_nan(df_ct, s.cols_ref_ct)
 
-drug_names = df_dr['drug'].to_list()
+# drug_names = df_dr['drug'].to_list()
