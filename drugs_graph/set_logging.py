f44636c0dd5302fafae6cfa8726cc4958db19022
nbouml <boum.nour19@gmail.com>
2022-10-30 13:45:29 +0100

    new: docstring for set_logging.py

--- drugs_graph/set_logging.py
+++ drugs_graph/set_logging.py
@@ -1,9 +1,27 @@
 import logging
 from os import remove
 from os.path import exists
+from typing import Any
 
 
-def setup_logger(logger_name, log_file, level=logging.WARNING):
+def setup_logger(logger_name: str, log_file: str, level: Any = logging.WARNING) -> None:
+    """
+    this method will set up the logger
+    Parameters
+    ----------
+    logger_name: str
+            the name of section in logger
+    log_file: str
+            the name of the file of the logger
+    level: Any
+            the level of log from logging package
+
+
+    Returns
+    -------
+    None
+
+    """
     # Erase log if already exists
     # if exists(log_file):
     #     remove(log_file)
